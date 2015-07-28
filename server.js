68926a72715621904c4f9882fe24a6dddcc53ef8
muhsinking <muhsinking@gmail.com>
2015-07-28 04:35:21 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var nodemailer = require("nodemailer");
+var fs = require('fs');
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+var jade = require('jade');
+var mandrillApi = JSON.parse(fs.readFileSync('private/apikey.json','utf8'));
+var app = express();
+
+app.set('view engine', 'jade');
+
+app.use(
+ sassMiddleware({
+     src: __dirname + '/sass',
+     dest: __dirname + '/public/css',
+     prefix:  '/css',
+//     outputStyle: 'compressed',
+     debug: true
+ })
+);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var mandrillTransport = require('nodemailer-mandrill-transport');
+
+var transporter = nodemailer.createTransport(mandrillTransport({
+	auth: {
+		apiKey: mandrillApi.key
+	}
+}));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/send', function(req,res){
+	
+	var mailOptions = {
+		from: req.query.from,
+		to : 'muhsinking@gmail.com',
+		subject : req.query.subject,
+		text : req.query.text
+	}
+
+	console.log(mailOptions);
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.end("errorrrrr");
+		}else{
+			console.log("Message sent!");
+			res.end("sent");
+		}
+	});
+});
+
+app.listen(8080,function(){
+	console.log("Express started on port 8080");
+});
